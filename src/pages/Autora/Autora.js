8ed168211dbce9f9ee9b5e236b06e670b67f1e02
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-12-15 20:27:55 -0300

    enviando alterações do arquivo autora

--- src/pages/Autora/Autora.js
+++ src/pages/Autora/Autora.js
@@ -7,7 +7,7 @@ import { DiHtml5 } from "react-icons/di"
 import { DiCss3 } from "react-icons/di" 
 import { DiJsBadge } from "react-icons/di" 
 import { DiReact } from "react-icons/di" 
-import { FaInstagram } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 
 const Autora = () => {
     return (
@@ -16,7 +16,7 @@ const Autora = () => {
             <h1 className='titulo-autora'>Sobre a Autora</h1>
             <img className='foto-autora' src={AutoraImagem} 
             alt='foto da autora'/>
-            <p className='paragrafo'>Sou nutricionista de formação e desenvolvedora front-end formada pelo bootcamp "Todas Em Tech - Turma 13" da reprograma. Embora tenha cursado nutrição e trabalhado na área, não me encontrei na profissão e decidi seguir meu coração e apreender minha segunda paixão, a tecnologia. Descobri então no front-end uma oportunidade de recomeço e desde então busco me aperfeiçoar e estudar todos os dias para atingir meus objetivos.</p>
+            <p className='paragrafo'>Me chamo Valenthina Temudo, tenho 25 anos e sou recifense. Sou nutricionista de formação e desenvolvedora front-end formada pelo bootcamp "Todas Em Tech - Turma 13" da reprograma. Embora tenha cursado nutrição e trabalhado na área, não me encontrei na profissão e decidi seguir meu coração e apreender minha segunda paixão, a tecnologia. Descobri então no front-end uma oportunidade de recomeço e desde então busco me aperfeiçoar e estudar todos os dias para atingir meus objetivos.</p>
            <section className="section-skills-redes">
            <section className="skills-container">
                 <p className="descricao">Tecnologias que domino:</p>
@@ -32,13 +32,13 @@ const Autora = () => {
             </section>
             <section className="section-redes">
                 <p className="paragrafo-redes">Minhas redes sociais:</p>
-                <a className='link-github' href="https://github.com/valenthinatemudo">
-                        <span className='span-link'></span> <DiGithubAlt size={30}/>
-                    </a>
-                    <a className='link-linkedin' 
+                <a className='link-github' href="https://   github.com/valenthinatemudo">
+                <DiGithubAlt size={30}/>
+                </a>
+                <a className='link-linkedin' 
                     href="https://www.linkedin.com/in/valenthina-temudo/">
-                        <span className='span-link'></span> <FaInstagram size={30}/>
-                    </a>
+                    <FaLinkedin size={30}/>
+                </a>
             </section>
            </section>
             <Footer />
