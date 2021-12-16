b6ca3f5719e6553d43be74f5306283b5bb39ab80
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-12-16 17:08:10 -0300

    finalizando projeto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,8 @@ import './header.styles.css'
 const Header = () => {
     return (
         <header classname='header'>
-            <div className='img-header'></div>
-            <div className='titulo-header'>
-                <h1 className='h1-header'>Alimentação básica para todos!</h1>
+            <div className='img-header'>
+                <h1 className='titulo-header'>Alimentação básica para todos!</h1>
             </div>
         </header>
     )
--- src/pages/Nutricionista/Nutri.js
+++ src/pages/Nutricionista/Nutri.js
@@ -7,13 +7,17 @@ const Nutri = () => {
         <div>
             <Menu />
                <header>
-                   <div className="img-nutri"></div>
-                    <h1 className="titulo-nutri">Nutricionista voluntário</h1>
+                   <div>
+                        <div className="img-nutri">
+                            <h1 className="titulo-nutri">Nutricionista voluntário</h1>
+                
+                            <p className="paragrafo-nutri">
+                                Quer se tornar um nutricionista voluntário atendendo e ajudando a população mais carente?
+                                Juntos podemos levar informações à essa população, explicando a eles o que seria uma alimentação mais saudável e adequada! Caso tenha interesse de participar desse projeto, faça seu cadastro aqui embaixo!
+                            </p>
+                        </div>
+                    </div>
                 </header>
-                <p className="paragrafo-nutri">
-                    Quer se tornar um nutricionista voluntário atendendo e ajudando a população mais carente?
-                    Juntos podemos levar informações à essa população, explicando a eles o que seria uma alimentação mais saudável e adequada! Caso tenha interesse de participar desse projeto, faça seu cadastro aqui embaixo!
-                </p>
                 <section className="section-frame">
                     <iframe className="frame" src="https://docs.google.com/forms/d/e/1FAIpQLSc_DW4ysLOnTyrBMKQVoXnGnEiKX6gkXHHJAZH-SGxqDbM9fA/viewform?embedded=true" width="640" height="947" frameborder="0"  title='myframe'></iframe>
                 </section>
