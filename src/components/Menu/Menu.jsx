364c3f285aa4e4d9590a3e0b7c85fa9f1a032313
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-12-16 20:17:58 -0300

    enviando alterações da pagina nutri

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom"
 import './menu.styles.css'
+import { BiHomeAlt } from 'react-icons/bi'
+import { FaWpforms } from 'react-icons/fa'
+import { BsPerson } from 'react-icons/bs'
 
 const Menu = () => {
     return (
         <ul className="menu">
             <li className="home">
-                <Link className="home-link" to='/'>Home</Link>
+                <Link className="home-link" to='/'> <BiHomeAlt className='icon-home' size={30}/> </Link>
             </li>
             <li className="nutri">
-                <Link className="nutri-link" to='/nutri'>Nutri</Link>
+                <Link className="nutri-link" to='/nutri'> <FaWpforms className="icon-nutri" size={30}/> 
+                </Link>
             </li>
             <li className="autora">
-                <Link className="autora-link" to='/autora'>Autora</Link>
+                <Link className="autora-link" to='/autora'> <BsPerson className="icon-autora" size={30}/>
+                </Link>
             </li>
         </ul>
     )
--- src/pages/Nutricionista/Nutri.js
+++ src/pages/Nutricionista/Nutri.js
@@ -1,6 +1,7 @@
 import Menu from "../../components/Menu/Menu";
 import Footer from "../../components/Footer/Footer";
 import './nutri.styles.css'
+import FotoForm from '../../assets/form.svg'
 
 const Nutri = () => {
     return (
@@ -8,18 +9,18 @@ const Nutri = () => {
             <Menu />
                <header>
                    <div>
-                        <div className="img-nutri">
                             <h1 className="titulo-nutri">Nutricionista voluntário</h1>
                 
                             <p className="paragrafo-nutri">
                                 Quer se tornar um nutricionista voluntário atendendo e ajudando a população mais carente?
                                 Juntos podemos levar informações à essa população, explicando a eles o que seria uma alimentação mais saudável e adequada! Caso tenha interesse de participar desse projeto, faça seu cadastro aqui embaixo!
                             </p>
-                        </div>
                     </div>
                 </header>
                 <section className="section-frame">
                     <iframe className="frame" src="https://docs.google.com/forms/d/e/1FAIpQLSc_DW4ysLOnTyrBMKQVoXnGnEiKX6gkXHHJAZH-SGxqDbM9fA/viewform?embedded=true" width="640" height="947" frameborder="0"  title='myframe'></iframe>
+                    <img className='foto-form' src={FotoForm} 
+            alt='foto da autora'/>
                 </section>
             
             <Footer />
