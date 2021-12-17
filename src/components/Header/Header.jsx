9a2ebbd334a68cbcf0f279038f060874837f8c09
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-12-16 22:13:08 -0300

    enviando alterações na home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,12 @@
 import './header.styles.css'
+import FotoMao from '../../assets/img1.jpg'
 
 const Header = () => {
     return (
         <header classname='header'>
-            <div className='img-header'>
-                <h1 className='titulo-header'>Alimentação básica para todos!</h1>
-            </div>
+            <h1 className='titulo-header'>Alimentação básica para todos!</h1>
+            <img src={FotoMao} align='left' className='foto-header' alt='foto de uma mao'/>
         </header>
     )
 }
-
 export default Header
