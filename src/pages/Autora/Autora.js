9a19c77239b81246e4c8baf688755840ee65b891
Valenthina Temudo <temudo.valenthina@gmail.com>
2021-12-16 21:27:27 -0300

    enviando modificações na pagina autora

--- src/pages/Autora/Autora.js
+++ src/pages/Autora/Autora.js
@@ -8,6 +8,8 @@ import { DiCss3 } from "react-icons/di"
 import { DiJsBadge } from "react-icons/di" 
 import { DiReact } from "react-icons/di" 
 import { FaLinkedin } from 'react-icons/fa'
+import RedeSocial from '../../assets/social.svg'
+import Skills from '../../assets/skill.svg'
 
 const Autora = () => {
     return (
@@ -19,6 +21,9 @@ const Autora = () => {
             <p className='paragrafo-autora'>Me chamo Valenthina Temudo, tenho 25 anos e sou recifense. Sou nutricionista de formação e desenvolvedora front-end formada pelo bootcamp "Todas Em Tech - Turma 13" da reprograma. Embora tenha cursado nutrição e trabalhado na área, não me encontrei na profissão e decidi seguir meu coração e apreender minha segunda paixão, a tecnologia. Descobri então no front-end uma oportunidade de recomeço e desde então busco me aperfeiçoar e estudar todos os dias para atingir meus objetivos.</p>
            <section className="section-skills-redes">
            <section className="skills-container">
+           <img className='icon-skill' src={Skills} 
+                alt='icone programando'/>
+                <div>
                 <p className="descricao">Tecnologias que domino:</p>
                 <div className="skills-box">
                     <p className="front-end">Front-End</p>
@@ -29,16 +34,21 @@ const Autora = () => {
                             <DiReact  size={40} />
                         </i>
                 </div>
+                </div>
             </section>
             <section className="section-redes">
-                <p className="paragrafo-redes">Minhas redes sociais:</p>
-                <a className='link-github' href="https://   github.com/valenthinatemudo">
-                <DiGithubAlt size={30}/>
-                </a>
-                <a className='link-linkedin' 
+                <img className='icon-redes' src={RedeSocial} 
+                alt='icone rede social'/>
+                <div>
+                    <p className="paragrafo-redes">Minhas redes sociais:</p>
+                    <a            className='link-github'          href="https://github.com/valenthinatemudo">
+                    <DiGithubAlt size={40}/>
+                    </a>
+                    <a className='link-linkedin' 
                     href="https://www.linkedin.com/in/valenthina-temudo/">
-                    <FaLinkedin size={30}/>
-                </a>
+                    <FaLinkedin size={40}/>
+                    </a>
+                </div>
             </section>
            </section>
             <Footer />
--- src/pages/Nutricionista/Nutri.js
+++ src/pages/Nutricionista/Nutri.js
@@ -7,15 +7,13 @@ const Nutri = () => {
     return (
         <div>
             <Menu />
-               <header>
-                   <div>
-                            <h1 className="titulo-nutri">Nutricionista voluntário</h1>
+               <header className="header-nutri">
+                    <h1 className="titulo-nutri">Nutricionista voluntário</h1>
                 
-                            <p className="paragrafo-nutri">
-                                Quer se tornar um nutricionista voluntário atendendo e ajudando a população mais carente?
-                                Juntos podemos levar informações à essa população, explicando a eles o que seria uma alimentação mais saudável e adequada! Caso tenha interesse de participar desse projeto, faça seu cadastro aqui embaixo!
-                            </p>
-                    </div>
+                    <p className="paragrafo-nutri">
+                         Quer se tornar um nutricionista voluntário atendendo e ajudando a população mais carente?
+                        Juntos podemos levar informações à essa população, explicando a eles o que seria uma alimentação mais saudável e adequada! Caso tenha interesse de participar desse projeto, faça seu cadastro aqui embaixo!
+                    </p>
                 </header>
                 <section className="section-frame">
                     <iframe className="frame" src="https://docs.google.com/forms/d/e/1FAIpQLSc_DW4ysLOnTyrBMKQVoXnGnEiKX6gkXHHJAZH-SGxqDbM9fA/viewform?embedded=true" width="640" height="947" frameborder="0"  title='myframe'></iframe>
